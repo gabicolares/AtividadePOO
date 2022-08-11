6915b473addd330fa16f28f32a6d4444528af7b4
gabicolares <gabicalicota@gmail.com>
2022-08-11 15:53:28 -0300

    Added all the necessary methods to Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,3 +1,21 @@
 public class Cliente {
-    
+    private String nome;
+    private String codigo;
+
+    public Cliente(String nome, String codigo) {
+        this.nome = nome;
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String toString() {
+        return String.format("Nome: %s%n Codigo: %s", nome, codigo);
+    }
 }
