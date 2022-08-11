c8241421341ce9411196960e1c6914127f9cf31e
gabicolares <gabicalicota@gmail.com>
2022-08-11 16:03:20 -0300

    Added some methods to Venda and Produto

--- Produto.java
+++ Produto.java
@@ -1,3 +1,11 @@
 public class Produto {
+    private int codigo;
+    private String nome;
+    private double preco;
     
+    public Produto(int umCodigo, double umPreco, String umNome) {
+        umCodigo = codigo;
+        umPreco = preco;
+        umNome = nome;
+    }
 }
--- Venda.java
+++ Venda.java
@@ -1,3 +1,19 @@
 public class Venda {
-    
+    private Cliente cliente;
+
+    public Venda(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public void inserir(Produto prod) {
+
+    }
+
+    public void inserir(int umaQtde, Produto prod) {
+
+    }
+
+    public double valorTotal() {
+        
+    }
 }
