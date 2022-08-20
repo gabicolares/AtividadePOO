c482d76fe9a34ca213f6511af41b9ead3a2b62d5
gabicolares <gabicalicota@gmail.com>
2022-08-19 23:43:08 -0300

    Added some things to ItemVenda

--- ItemVenda.java
+++ ItemVenda.java
@@ -1,8 +1,21 @@
 public class ItemVenda {
     private int quantidade;
+    private Produto prod;
 
     public ItemVenda(int umaQtde, Produto umProd) {
         umaQtde = quantidade;
+        umProd = prod;
     }
+
+    public double getValor() {
+        double val = quantidade * prod.getPreco();
+        
+        return val;
+    }
+
+    public String toString() {
+        return String.format("Valor: %n");
+    }
+
 
 }
